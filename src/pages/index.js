f496e50fd19322d8e946e7f7502c5c6c77dd223f
Jack Humphrey <youknowjack>
2019-12-03 14:42:45 +0000

    update home page, remove image and fix link

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Data Analytics for GCD</h1>
-    <p>Details coming soon!</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/doc/imhotep-sample-queries/">Imhotep Sample Queries</Link>
+    <SEO title="Data Analytics for GCD comics.org" />
+    <h1>Data Analytics for <a href="https://www.comics.org">GCD</a></h1>
+    <Link to="/imhotep-samples">Imhotep Sample Queries</Link>
   </Layout>
 )
 
