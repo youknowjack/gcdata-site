19c2755646ad71952ac3b84c81eab2527e50add0
Jack Humphrey <jack@indeed.com>
2019-12-24 09:05:32 -0600

    open comics.org in a new window

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Data Analytics for GCD comics.org" />
-    <h1>Data Analytics for <a href="https://www.comics.org">GCD</a></h1>
+    <h1>Data Analytics for <a href="https://www.comics.org" target="_blank" rel="noopener noreferrer">GCD</a></h1>
     <div><Link to="/athena-samples">Athena Sample Queries</Link></div>
     <div><Link to="/imhotep-samples">Imhotep Sample Queries</Link></div>
   </Layout>
