ecb8a77cc9252ac7b89cae306b7ee245d52d4b5e
Jack Humphrey <2129899+youknowjack@users.noreply.github.com>
2020-08-02 23:16:41 -0500

    Update to latest snapshot

--- src/pages/athena-samples.js
+++ src/pages/athena-samples.js
@@ -6,7 +6,7 @@ class AthenaSampleQuery extends React.Component {
   render() {
     const samples = this.props.data.allMarkdownRemark.edges
     const sqlurl = "https://athena.gcdata.org/embed/";
-    const snapshot = 20200715;
+    const snapshot = 20200801;
 
     return (
       <Layout>
