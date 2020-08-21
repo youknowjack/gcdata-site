e4d8aa2fbbe4f4b31745dccfd97269a11c3b8609
Jack Humphrey <jack@indeed.com>
2020-08-20 22:27:16 -0500

    Add schema information on About page and link to it from sample queries and index

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Data Analytics for GCD comics.org" />
     <h1>Data Analytics for <a href="https://www.comics.org" target="_blank" rel="noopener noreferrer">GCD</a></h1>
-    <div><Link to="/redash-samples">Redash Sample Queries (using Presto/Hive/Parquet)</Link></div>
+    <div><Link to="/redash-samples">Redash Sample Queries (using Presto/Hive/Parquet)</Link> (<Link to="/about-redash/">about this dataset</Link>)</div>
     <div><Link to="/imhotep-samples">Imhotep Sample Queries</Link></div>
   </Layout>
 )
