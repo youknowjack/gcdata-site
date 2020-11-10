afa6e00c6b65e6b4b3ce1721e9c87fed28331391
Jack Humphrey <jack@indeed.com>
2020-11-10 13:55:01 -0600

    change explanation since the cluster has been shut down

--- src/pages/imhotep-samples.js
+++ src/pages/imhotep-samples.js
@@ -10,22 +10,11 @@ class SampleQuery extends React.Component {
       <Layout>
         <div className="doc-content">
           <h1>Sample Imhotep Queries</h1>
-          <p>Each sample query below includes the IQL along with a link to run the query on the
-          Imhotep cluster. In each query, <code>&lt;start-date&gt;</code> and <code>&lt;end-date&gt;</code> reference the
-          date of the GCD data dump. Each link uses dates for the most recent data dump uploaded into Imhotep.
-          Read more <a href="/about-imhotep">about Imhotep for GCD.</a></p>
-          <p>You are encouraged to use the IQL webapp to try variations these sample queries and come up with new ones. 
-          Note that the Imhotep cluster is currently running on a single EC2 t2.small spot instance, so performance 
-          could be better and occasionally the webapp may become unavailable. If you notice a problem, please open
-          a <a href="https://github.com/youknowjack/gcd-etl/issues/new" target="_blank"
-               rel="noopener noreferrer">Github issue</a> in <code>gcd-etl</code> or
-          email <a href="https://mailhide.io/e/T8xal" onclick="mailhidepopup=window.open('https://mailhide.io/e/T8xal','mailhidepopup','width=580,height=635'); return false;">a......@gcdata.org</a>.</p>
-          <p>If you find a great new query that we could add to this page, please open
-          a <a href="https://github.com/youknowjack/gcdata-site/issues/new" target="_blank"
-               rel="noopener noreferrer">Github issue</a> in <code>gcdata-site</code>.
-          Pull requests are also welcome -- adding a query is as easy
-            as <a href="https://github.com/youknowjack/gcdata-site/blob/master/src/imhotep-samples/README.md"
-               target="_blank" rel="noopener noreferrer">adding a single Markdown file</a> in the project.</p>
+          <p>The GCD Imhotep cluster is not currently available. The queries below
+          illustrate the IQL versions of the <a href="/redash-samples" rel="noopener noreferrer">Presto
+          SQL queries</a> served via Redash. In each query, <code>&lt;start-date&gt;</code> and 
+          <code>&lt;end-date&gt;</code> reference the date of the GCD data dump. Each link uses dates for the most
+          recent data dump that would be uploaded to and available via Imhotep.</p>
 
           {samples.map(sample => (
             <div id={sample.node.frontmatter.shortlink}>
