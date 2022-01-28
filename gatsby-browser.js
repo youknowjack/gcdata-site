718bea93158dfc265e07651e0b860a11ac530390
Jack Humphrey <jack@indeed.com>
2022-01-27 22:28:36 -0600

    fix mixpanel tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,22 @@
 require("prismjs/themes/prism.css")
 
+exports.track = () => {
+    window.mixpanel.track(document.location.pathname);
+    var trackedLinks = document.getElementsByClassName("mixpanel-track");
+    for (var i = 0; i < trackedLinks.length; i++) {
+      var prefix = trackedLinks[i].dataset["mptrackPrefix"];
+      var id = trackedLinks[i].id;
+      window.mixpanel.track_links("#" + id, prefix + "-" + id);
+    }
+}
+
 exports.onInitialClientRender = () => {
-  document.mixpanel.track(document.location.pathname);
-  var trackedLinks = document.getElementsByClassName("mixpanel-track");
-  for (var i = 0; i < trackedLinks.length; i++) {
-    var prefix = trackedLinks[i].dataset["mptrackPrefix"];
-    var id = trackedLinks[i].id;
-    document.mixpanel.track_links("#" + id, prefix + "-" + id);
+  window.init_mixpanel();
+  exports.track();
+}
+
+exports.onRouteUpdate = () => {
+  if (window.mixpanel) {
+    exports.track();
   }
 }
