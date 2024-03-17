8228ba89043dbdef335ce6efb1706dbb90b031a6
Jack Humphrey <jack@indeed.com>
2024-03-16 19:25:20 -0500

    update to latest snapshot

--- src/pages/redash-samples.js
+++ src/pages/redash-samples.js
@@ -6,7 +6,7 @@ class RedashSampleQuery extends React.Component {
   render() {
     const samples = this.props.data.allMarkdownRemark.edges
     const sqlurl = "https://redash.gcdata.org/embed/";
-    const snapshot = 20240215;
+    const snapshot = 20240315;
 
     return (
       <Layout>
